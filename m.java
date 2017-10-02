06ebd80c7e62e5098a01840db4d908ab91e7b0ab
Owen Li <TLi3@wyomingseminary.org>
2017-10-02 16:14:21 -0400

    Add files via upload

---
+++ m.java
@@ -0,0 +1,42 @@
+package calculator;
+
+public class m{
+
+	public static final double PI = Math.PI;
+	
+	public static final double e = Math.E;
+	
+	public static double abs(double d){
+		return Math.abs(d);
+	}
+	
+	public static double round(double n, int digit){
+		return Math.round(n*Math.pow(10, digit))/Math.pow(10, digit);
+	}
+	
+	public static String toString(double n){
+		return new Double(n).toString();
+	}
+	
+	public static int digitAt(int num, int digit){
+		
+		num /= Math.pow(10, digit-1);
+		
+		num = (int)Math.ceil(abs(num));
+		
+		return num%10;
+		
+	}
+	
+	public static double integrate(String str, String as, String bs){
+		double count = 0, a = Fx.evaluate(as, 0),b = Fx.evaluate(bs, 0);
+		for(double i=0;i<10000;i++)
+			count += Fx.evaluate(str, a+(b-a)*i/10000);
+		count *= (b-a)/10000;
+		return count;
+	}
+	
+	public static void main(String[]args){
+		System.out.println(round(integrate("x^2","0","1"),6));
+	}
+}
